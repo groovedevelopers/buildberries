6681cbef15f66bdd10f749e18e4265841830c947
bilytaiba <benjotaiba@gmail.com>
2021-11-23 16:27:01 +0100

    help center page

--- interface/src/components/helpCenterPage.js
+++ interface/src/components/helpCenterPage.js
@@ -1,10 +1,13 @@
+import HelpImage1 from '../assets/images/template-1.png';
+import HelpImage2 from '../assets/images/template-2.png';
+import RightArrow from '../assets/images/icons/right-arrow.svg';
 
 const HelpCenterPage = () => {
     return (
         <div className="container help-center-page">
 			<div className="d-flex flex-column help-center-page-body">
 			
-				<div className="d-flex flex-column help-center-heading">
+				<div className="help-center-page-heading">
 					<div>
 						<h2>Help center</h2>
 						<p>
@@ -14,32 +17,58 @@ const HelpCenterPage = () => {
                             
                         <form>
                             <input type="text" className="form-control" placeholder="Search for your question" />
+                            <span className="text-center"><i className="fa fa-search"></i></span>
                         </form>
 					</div>
 				</div>
 
-                <div>
+                <div className="articles">
                     <h3>Articles</h3>
-                    <div className="d-flex flex-row justify-content-between">
+                    <div className="d-flex flex-row justify-content-between article">
                         <div>
-                            {/* SPACE FOR IMAGE */}
+                            <img src={HelpImage1} alt="Article" />
                         </div>
-                        <div>
-                            <h4>How to use: Business growth tools</h4>
-                            <p>
-                                Aliqua id fugiat nostrud irure ex duis
-                                ea quis id quis ad et. Sunt qui esse
-                                pariatur duis deserunt mollit dolore...
-                            </p>
+                        <div className="d-flex flex-column justify-content-between article-body">
+                            <div>
+                                <h4>How to use: Business growth tools</h4>
+                                <p>
+                                    Aliqua id fugiat nostrud irure ex duis
+                                    ea quis id quis ad et. Sunt qui esse
+                                    pariatur duis deserunt mollit dolore...
+                                </p>
+                            </div>
                             <div className="d-flex flex-row justify-content-between">
-                                <span>2 days ago by Avi sahra</span>
-                                <span>View article</span>
+                                <span><b>2 days</b> ago by <b>Avi sahra</b></span>
+                                <a href="/">View article <img src={RightArrow} alt="Right arrow" /></a>
                             </div>
                         </div>
                     </div>
+                    <div className="d-flex flex-row justify-content-between article">
+                        <div>
+                            <img src={HelpImage2} alt="Article" />
+                        </div>
+                        <div className="d-flex flex-column justify-content-between article-body">
+                            <div>
+                                <h4>All new SEO features</h4>
+                                <p>
+                                    Aliqua id fugiat nostrud irure ex duis
+                                    ea quis id quis ad et. Sunt qui esse
+                                    pariatur duis deserunt mollit dolore...
+                                </p>
+                            </div>
+                            <div className="d-flex flex-row justify-content-between">
+                                <span><b>2 days</b> ago by <b>Avi sahra</b></span>
+                                <a href="/">View article <img src={RightArrow} alt="Right arrow" /></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="text-center more-articles">
+                        <a href="/">Load more articles <img src={RightArrow} alt="Right arrow" /></a>
+                    </div> 
                 </div>
 
-                <div>
+                <div className="answers">
                     <h4>Find answers</h4>
                     <div className="d-flex flex-row justify-content-between">
                         <div className="accordion accordion-flush" id="accordionFlushExample">
